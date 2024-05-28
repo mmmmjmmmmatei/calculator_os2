b2f1a4534925a4dcacbfc9dfa1a1106cdd228d99
915_Muresan_Matei <91517363+mmmmjmmmmatei@users.noreply.github.com>
2024-05-28 19:02:43 +0300

    added threads

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
+#include <malloc.h>
+
+struct threadArgs{
+    float n1, n2;
+    int op_id;
+};
 
 void display_menu(){
     printf("1. Add\n");
@@ -7,17 +14,39 @@ void display_menu(){
     printf("3. Multiply\n");
     printf("4. Divide\n");
     printf("5. Exponentiate\n");
+    printf("6. Display All\n");
     printf("Please select a command: ");
 }
 
-int main(){
+void print_operation(float n1, float n2, int op_id){
+    if(op_id == 1)
+        printf("Result of addition is: %.2f\n", n1+n2);
+    else if(op_id == 2)
+        printf("Result of subtraction is: %.2f\n", n1-n2);
+    else if(op_id == 3)
+        printf("Result of multiplication is: %.2f\n", n1*n2);
+    else if(op_id == 4)
+        printf("Result of division is: %.2f\n", n1/n2);
+    else if(op_id == 5)
+        printf("Result of exponentiation is: %.2f\n", powf(n1,n2));
+}
+
+void* print_withThreads(void* args){
+    struct threadArgs *currArgs = (struct threadArgs*)args;
+    print_operation(currArgs->n1, currArgs->n2, currArgs->op_id);
+    free(args);
+    return NULL;
+}
+
+int main(int argc, char *argv[]){
+    pthread_t t[5];
     int com = 0;
     float n1, n2;
     display_menu();
     scanf("%d", &com);
     printf("\n");
-    while(com < 1 || com > 5){
-        printf("Please enter a number between 1 and 5: ");
+    while(com < 1 || com > 6){
+        printf("Please enter a number between 1 and 6: ");
         scanf("%d", &com);
         printf("\n");
     }
@@ -27,22 +56,19 @@ int main(){
     printf("Enter second number: ");
     scanf("%f", &n2);
     printf("\n");
-    switch(com){
-        case 1:
-            printf("Result of addition is: %.2f\n", n1+n2);
-            break;
-        case 2:
-            printf("Result of subtraction is: %.2f\n", n1-n2);
-            break;
-        case 3:
-            printf("Result of multiplication is: %.2f\n", n1*n2);
-            break;
-        case 4:
-            printf("Result of division is: %.2f\n", n1/n2);
-            break;
-        case 5:
-            printf("Result of exponentiation is: %.2f\n", powf(n1,n2));
-            break;
+    if(com >= 1 && com <= 5)
+        print_operation(n1, n2, com);
+    else {
+        for(int i = 0; i < 5; i++) {
+            struct threadArgs *args = malloc(sizeof(struct threadArgs));
+            args->n1 = n1;
+            args->n2 = n2;
+            args->op_id = i + 1;
+            pthread_create(&t[i], NULL, print_withThreads, (void *) args);
+        }
+        for(int i = 0; i < 5; i++) {
+            pthread_join(t[i], NULL);
+        }
     }
     return 0;
 }
