55d2d8c63b1c962c1dc411db83764a2a053024d0
slidewhistle444 <mateimuresan2702@gmail.com>
2024-05-28 19:54:07 +0300

    cmake

---
+++ bin/CMakeFiles/calculator_os2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_os2.dir/main.c.o"
+  "CMakeFiles/calculator_os2.dir/main.c.o.d"
+  "calculator_os2"
+  "calculator_os2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculator_os2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
